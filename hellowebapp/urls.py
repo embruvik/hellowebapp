162d7219362e1805cd33e2733d114a432c6343d0
em <ermabr@hotmail.com>
2017-02-08 19:33:19 +0100

    About and Contact pages added

--- hellowebapp/urls.py
+++ hellowebapp/urls.py
@@ -15,11 +15,15 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-
+from django.views.generic import TemplateView
 from collection import views
 
 urlpatterns = [
     url(r'^$', views.index, name='home'),
-    url(r'^a[0-9]*', views.index2, name='home2'),
+    url(r'^a[0-9]a*', views.index2, name='home2'),
+
+    url(r'^about/$', TemplateView.as_view(template_name='about.html'), name='about'),
+    url(r'^contact/$', TemplateView.as_view(template_name='contact.html'), name='contact'),
+
     url(r'^admin/', admin.site.urls),
 ]
