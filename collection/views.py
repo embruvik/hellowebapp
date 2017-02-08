b425aa29cdfd307b0ee3344fdd2732f032a9e9a8
em <ermabr@hotmail.com>
2017-02-08 22:14:10 +0100

    Form to edit things

--- collection/views.py
+++ collection/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+
+from collection.forms import ThingForm
 from collection.models import Thing
 
 # Create your views here.
@@ -17,4 +19,23 @@ def thing_detail(request, slug):
     thing = Thing.objects.get(slug=slug)
 
     return render(request, 'things/thing_detail.html', {'thing': thing,})
+
+def edit_thing(request, slug):
+    thing = Thing.objects.get(slug=slug)
+    # set the form we are using
+    form_class = ThingForm
+
+    # if we are coming to this view from a submitted format
+    if request.method == 'POST':
+        # grab data form the submitted form and apply to the format
+        form = form_class(data=request.POST, instance=thing)
+        if form.is_valid():
+            form.save()
+            return redirect('thing_detail',slug=thing.slug)
+    else:
+        #otherwise just create the form
+        form = form_class(instance=thing)
     
+    retobj = render(request,'things/edit_thing.html', {'thing': thing, 'form': form,})
+    return retobj
+
